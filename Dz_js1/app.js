255d30b6cbabb948c48141475b8762bc7ce53064
Irina Karavashkina <dd200383karavashkina@gmail.com>
2020-06-23 22:46:09 +0300

    домашнее задание по Js

---
+++ Dz_js1/app.js
@@ -0,0 +1,38 @@
+/*Дз_1_JavaScript*/
+let message = "hello";
+var number = 123;
+let name = "Iryna";
+var string = 'строка';
+var boolTrue = true;
+var boolFalse = false;
+var testBool = Boolean(1);
+
+function test1(){
+    alert('Привет');
+    alert('мир')
+}
+
+function test2(){
+    alert(3+
+        1+
+        2)
+}
+
+function test3(){
+    alert( `hello ${"name"}` );
+}
+
+function test4(){
+    alert( "Я учу JavaScript" );
+}
+function test5(){
+    let name = prompt("Ваше имя?", "");
+    alert(name);
+}
+function test6(){
+var a=true;
+alert(a);
+}
+function test7(){
+alert(1/0);
+}
---
+++ Dz_js2/app.js
@@ -0,0 +1,61 @@
+/*Дз_2_JavaScript*/
+
+function test1() {
+    var text = prompt('Каково \"официальное\" название JavaScript?');
+    if (text == 'ECMAScript') alert('Верно!')
+    else alert('Не знаете? \"ECMAScript\"!')
+}
+
+function test2() {
+    var i = + prompt("Введите число");
+    console.log(typeof i)
+    if ( i > 0) {
+        alert('1');
+    }
+    else if (i < 0) {
+        alert('-1');
+        ;
+    }
+    else
+    {
+        alert('0');
+    }
+}
+
+function test3() {
+    let text = prompt('Кто пришел?');
+    console.log(text);
+    if (text === 'Admin'){
+    let pass = prompt('Введите пароль:');
+    if (pass==='Чёрный Властелин') {
+        alert('Добро пожаловать!')}
+    else if (pass == null) {
+        alert('Вход отменен')}
+    else {
+        alert('Пароль неверен')}
+    }
+    else if (text == null) {
+        alert('Вход отменен')}
+    else {
+        alert('Я Вас не знаю!')}
+}
+
+function test4() {
+    var i = 0
+    while(i < 3){
+        console.log(i);
+        alert('номер ' + i + ' !');
+        i++;
+    }
+}
+
+function test5() {
+    do {
+        var text = prompt('Введите число больше 100: ');
+        if (text == null) {
+            alert('Вход отменен');
+            break;
+        }
+    }
+    while (text < 100);
+}
